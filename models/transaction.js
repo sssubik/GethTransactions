acf99cd5b32c5a293ac0e4289454d0816f8217d7
ssubikshrestha <subik.shrestha@fusemachines.com>
2020-05-11 13:46:27 +0545

    Implemented writing txhash to file

--- models/transaction.js
+++ models/transaction.js
@@ -1,9 +1,14 @@
+var Tx = require('ethereumjs-tx').Transaction
+const Web3 = require('web3')
+const account1 = "0x06683f102Bf9A98235589dE43Cb30bEfaC8cfD57"
+const web3 = new Web3("https://ropsten.infura.io/v3/05ac82a3f5f34072ad44566c73b2cc24")
 const fs = require('fs');
 const path = require('path');
+const privateKey1 = Buffer.from(process.env.PRIVATE_KEY_1,'hex')
 const p = path.join(
     path.dirname(process.mainModule.filename),
     'data',
-    'products.json'
+    'transaction.json'
   );
 
   const getTransactionsFromFile = cb =>{
@@ -16,20 +21,20 @@ const p = path.join(
           }
       })
   }
-const sendSignedTransaction = (privateKey, fromAccount, cb) => {
+const sendSignedTransaction = (toAccount, cb) => {
+
     web3.eth.getTransactionCount(account1).then( async (count,error) =>{
        const txObject = { 
            nonce: web3.utils.toHex(count),
-           to: account2,
+           to: toAccount,
            gasLimit: web3.utils.toHex(23000),
            gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
            data: web3.utils.toHex("data")
        }
    
        
-      //Sign the transaction
-       const tx = new Tx(txObject,{"chain":"ropsten"})
-       tx.sign(privateKey1)
+        const tx = new Tx(txObject,{"chain":"ropsten"})
+        tx.sign(privateKey1)
    
        const serializedTransaction = tx.serialize()
        const raw = '0x' + serializedTransaction.toString('hex')
@@ -41,14 +46,18 @@ const sendSignedTransaction = (privateKey, fromAccount, cb) => {
                    "txHash": txHash
                }
                if(!err){
-                   cb(JSON.parse(transaction))
+                   try{
+                   cb(transaction)
+                   }catch(error){
+                       console.log(error)
+                   }
                }
                else{
                    throw "noTransaction";
                } 
            }catch(error){
                        if(error="noTransaction"){
-                           console.log(err)
+                           console.log(error)
                        }
    
            }
@@ -62,10 +71,10 @@ const sendSignedTransaction = (privateKey, fromAccount, cb) => {
 }
 module.exports = class Transaction{
       
-    static sendTransaction(privateKey, fromAccount){
-        sendSignedTransaction((privateKey, fromAccount, txHash) =>{
-            fs.writeFile(p, JSON.stringify(txHash),err =>{
-                console.log(err);
+    static sendTransaction(toAccount){
+        sendSignedTransaction(toAccount,txHash => {
+            fs.appendFile(p, JSON.stringify(txHash)+",",err =>{
+                console.log(txHash)
             })
         })
     }
